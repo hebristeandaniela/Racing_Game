8963e2417c48ccf30e0dd2a2fa80924ae0982160
Hebristean Daniela <daniela.hebristean22@gmail.com>
2023-04-21 20:28:10 +0300

    declaring methods

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -9,4 +9,17 @@ public class Vehicle {
     double maxSpeed; //viteza maxima
     boolean damaged; //masina stricata? true or false
     String color;
+
+    public double accelerate(double speed, double durationInHours){
+        System.out.println(name + "is accelerating with " + speed + "km/h for" + durationInHours + "h");
+    double distance = speed * durationInHours;
+    totalTraveledDistance += distance;
+        System.out.println("Total traveled distance for vehicle" + name + ":" + totalTraveledDistance);
+        double usedFuel = distance * mileage / 100;
+        fuelLevel -= usedFuel;
+        System.out.println("Remaining fuel for vehicle" + name + ":" + fuelLevel);
+
+        return distance;
+    }
+
 }
