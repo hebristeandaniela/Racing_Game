9be5bfefc1b0e6b25d979aa7ab6d721127cf7f6c
Hebristean Daniela <daniela.hebristean22@gmail.com>
2023-04-21 19:58:11 +0300

    studied inheritance

---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//"is-a" relationship (inheritance)
+public class Vehicle {
+    String name;
+    double fuelLevel; //combustibil
+    double mileage; //consum mediu
+    double totalTraveledDistance; //distanta parcursa
+    double maxSpeed; //viteza maxima
+    boolean damaged; //masina stricata? true or false
+    String color;
+}
