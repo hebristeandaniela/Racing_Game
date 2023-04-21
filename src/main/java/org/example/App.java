7694350ebb2f00b3e7c0e5070de4d51257439a79
Hebristean Daniela <daniela.hebristean22@gmail.com>
2023-04-21 21:15:54 +0300

    added mechanic and method receiving object param

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -59,7 +59,10 @@ public class App
 
         double distanceForCar1 = carReference.accelerate(60,1);
         double distanceForCar2 = car2.accelerate(100, 1);
-
+        System.out.println("Engine manufacturer before repair:" + carReference.engine.manufacturer);
+        Mechanic mechanic = new Mechanic();
+        mechanic.repair(carReference);
+        System.out.println("Engine manufacturer after repair:" + carReference.engine.manufacturer);
     }
 }
 
